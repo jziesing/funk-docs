02ebf8cff4a5f48b45a82b9d2b379588accc1907
Jack Ziesing <jziesing@gmail.com>
2021-11-09 13:39:47 -0800

    five

--- force-app/main/default/lwc/makeaccountlwc/makeaccountlwc.js
+++ force-app/main/default/lwc/makeaccountlwc/makeaccountlwc.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
 
-export default class Makeaccountlwc extends LightningElement {}
+
+import { LightningElement, track } from 'lwc';
+import addAccount from '@salesforce/apex/funkinvoker.addAccount';
+
+export default class Makeaccountlwc extends LightningElement {
+    clickedButtonLabel;
+	@track contacts;
+	@track accounts;
+    @track error;
+
+	amount = 0;
+
+    handleAmountChange(e) {
+        this.amount = e.detail.value;
+    }
+
+
+	handleLoad() {
+		console.log('is clicked');
+		for(var i=0; i<this.amount; i++) {
+			addAccount()
+				.then(result => {
+					console.log(result);
+					this.contacts = result;
+				})
+				.catch(error => {
+					console.log(error);
+					this.error = error;
+				});
+		}
+	}
+	
+}
